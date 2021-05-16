d9a36a1bc55d5bff3b7184fbc9f7ba86b0adae1d
niealda <danieluchechukwuchuks1@gmail.com>
2021-05-16 14:35:38 -0700

    listed  robo

---
+++ src/Hello.js
@@ -0,0 +1,18 @@
+import './Hello.css';
+
+
+const Hello = () => {
+  return (
+      <div className ="f2 tc" >
+
+        <h1> Chubuk Initiative  </h1>,
+       {/* <p> { this.props.greeting } </p> */}
+
+      </div>
+        
+  );
+}
+
+export default Hello;
+
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(< Hello />, document.getElementById('root'));
+//ReactDOM.render(< App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
