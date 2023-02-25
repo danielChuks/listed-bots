dfb3c2462e21c40c12b5af252ba0e1ad31a43705
Daniel Chuks <danieluchechukwuchuks1@gmail.com>
2023-02-25 13:15:34 +0100

    functional component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,58 +1,54 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
 import SearchBox from '../components/SearchBox';
+import axios from 'axios';
 import './App.css';
 
+const App = () => {
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchFiled] = useState('');
+    const [loading, setLoading] = useState(false);
 
- class App extends Component {
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
+    useEffect(() => {
+        fetchData();
+    }, [searchfield]);
+
+    const fetchData = async () => {
+        try {
+            setLoading(true)
+            const url = `https://jsonplaceholder.typicode.com/users`;
+            const response = await axios.get(url);
+            setLoading(false);
+            setRobots(response.data);
+        } catch (error) {
+            console.log(error);
         }
-        
-        } 
-  //  we dont need arrow functions when using //componentDidMount because its a javascript function...
-componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-
-    .then( users => this.setState({ robots: users })); 
-    
-}
+    };
     //this was made up to create an eventfunction
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-        //(event.target.value) will give us the value of what we input on the seacrhbox.  
-    }
+    const onSearchChange = (event) => {
+        setSearchFiled(event.target.value);
+    };
 
-    render() {
-        const { robots , searchfield} = this.state;
-        const filteredRobots = robots.filter( robot => {
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
+    const filteredRobots = robots.filter((robot) => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
 
-        if (robots.length === 0)
-            return <h2> Loading.... </h2>   
-        else {
-           return (
-            <div className ='tc'>  
-                <h1 className='f1'>Gentle Bots</h1>
-                <SearchBox searchChange = {this.onSearchChange }/>
-                <Scroll>
-                    <CardList robots={ filteredRobots } />      
-                </Scroll>                     
-                
-            </div>
-                  );
-         }       
-        }
-    }
-    
- 
-
-
+    return (
+        <div className="tc">
+            {loading ? (
+                <h1> Loading.... </h1>
+            ) : (
+                <div>
+                    <h1 className="f1">Gentle Bots</h1>
+                    <SearchBox searchChange={onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default App;
